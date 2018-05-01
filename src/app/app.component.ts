40d745d049a57120e740e4b6459d81e2c772d465
raul1991 <supremeanitabawa@gmail.com>
2018-05-01 21:57:05 +0200

    Add support for put request
    closes #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,62 +9,78 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 })
 export class AppComponent {
   title = 'app';
-  url = 'http://127.0.0.1:8200/v1/secret/foo';
+  baseUrl = 'http://127.0.0.1:8200/v1/secret/';
+  key; // used during a put request
+  value; // used during a put request
   token;
   headers;
   result;
-  action: String = 'Get';
-  actions = [
+  supportedVerbs = [
     'Get',
     'Put',
-    'Post',
     'List',
     'Delete'
   ];
+  verb: String = this.supportedVerbs[0];
   constructor(private http: HttpClient) { }
 
-  performAction(action: String) {
+  performAction(selectedVerb: String) {
     this.result = "";
-    console.log("Doing a get @" + this.url);
-    switch (action) {
+    var secret = {key: this.getAbsolutePath(this.key)};
+    switch (selectedVerb) {
       case "Get":
-        this.doGet();
+        this.doGet(secret);
         break;
       case "List":
-        this.doList();
-        break;
-      case "Post":
-        this.doPost();
+        this.doList(secret);
         break;
       case "Delete":
-        this.doDelete();
+        this.doDelete(secret);
         break;
       case "Put":
-        this.doPut();
+        secret['value'] = this.value;
+        this.doPut(secret);
         break;
       default:
         // code...
         break;
     }
   }
-
-  doPut() {
-    
+  
+  getAbsolutePath(relativePath: string) {
+    return this.removeTrailingSlashIfExists(this.baseUrl).concat("/").concat(relativePath);
   }
 
-  doGet() {
-    this.http.get(this.url, {
+  removeTrailingSlashIfExists(path: string) {
+    if (path.endsWith("/")) {
+      var idx = path.lastIndexOf("/");
+      return path.substring(0, idx);
+    }
+    return path;
+  }
+
+  doPut(secret) {
+    this.http.put(secret.key, {"value":secret.value}, {
       headers: new HttpHeaders({ 'X-Vault-Token': this.token })
     }).subscribe((res) => {
-        this.result = JSON.stringify(res['data'], null, 2);
+        this.result = secret.value + " was saved with the key -> " + this.key;
     }, (err) => {
-        console.log(err);
         this.result = JSON.stringify(err.statusText);
     });
   }
 
-  doList() {
-    this.http.get(this.url+"?list=true", {
+  doGet(secret) {
+    this.http.get(secret.key, {
+      headers: new HttpHeaders({ 'X-Vault-Token': this.token })
+    }).subscribe((res) => {
+        this.result = JSON.stringify(res['data'], null, 2);
+    }, (err) => {
+        this.result = JSON.stringify(err.statusText);
+    });
+  }
+
+  doList(secret) {
+    this.http.get(secret.key + "?list=true", {
       headers: new HttpHeaders({ 'X-Vault-Token': this.token })
     }).subscribe((res) => {
         this.result = JSON.stringify(res['data'].keys, null, 2);
@@ -73,8 +89,8 @@ export class AppComponent {
     });
   }
 
-  doDelete() {
-    this.http.delete(this.url, {
+  doDelete(secret) {
+    this.http.delete(secret.key, {
       headers: new HttpHeaders({ 'X-Vault-Token': this.token })
     }).subscribe((res) => {
         this.result = 'Secret deleted if it existed';
@@ -83,11 +99,11 @@ export class AppComponent {
     });
   }
 
-  doPost() {
-    
+  isPut() {
+    return this.verb === "Put"
   }
-
+  
   setAction(action: String) {
-    this.action = action;
+    this.verb = action;
   }
 }
